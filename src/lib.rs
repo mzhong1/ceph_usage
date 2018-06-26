1f1aab29d2782719fa2a3cafc6fe04cea2fef063
Chris Holcombe <xfactor973@gmail.com>
2018-06-26 13:00:13 -0700

    Upgrade ceph dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@ use std::path::Path;
 use std::str::FromStr;
 
 use ceph::ceph::*;
-use ceph::cmd::{PoolOption, osd_pool_get};
-use ceph::rados::{rados_t, Struct_rados_cluster_stat_t, Struct_rados_pool_stat_t};
+use ceph::cmd::{osd_pool_get, PoolOption};
+use ceph::rados::{Struct_rados_cluster_stat_t, Struct_rados_pool_stat_t};
 
 #[derive(Debug)]
 pub struct PoolInfo {
@@ -22,10 +22,8 @@ pub struct UsageInfo {
     pub pool_usage: Vec<PoolInfo>,
 }
 
-pub fn get_pool_size(handle: rados_t, pool: &str) -> Result<u32, String> {
-    let pool_size_str = osd_pool_get(handle, &pool, &PoolOption::Size).map_err(
-        |e| e.to_string(),
-    )?;
+pub fn get_pool_size(handle: &Rados, pool: &str) -> Result<u32, String> {
+    let pool_size_str = osd_pool_get(handle, &pool, &PoolOption::Size).map_err(|e| e.to_string())?;
     if let Some(s) = pool_size_str.split_whitespace().last() {
         let pool_size = u32::from_str(&s).map_err(|e| e.to_string())?;
         debug!("pool_size: {}", pool_size);
@@ -41,28 +39,26 @@ pub fn get_cluster_usage(user: &str, conf_file: &Path) -> Result<UsageInfo, Stri
     let mut pool_usage: Vec<PoolInfo> = Vec::new();
 
     debug!("Connecting to ceph");
-    let h = connect_to_ceph(user, &format!("{}", conf_file.display()))
-        .map_err(|e| e.to_string())?;
+    let h = connect_to_ceph(user, &format!("{}", conf_file.display())).map_err(|e| e.to_string())?;
 
     debug!("Running stat against the cluster");
-    let cluster_stats = rados_stat_cluster(h).map_err(|e| e.to_string())?;
+    let cluster_stats = h.rados_stat_cluster().map_err(|e| e.to_string())?;
 
     debug!("Listing pools");
-    let pools = rados_pools(h).map_err(|e| e.to_string())?;
+    let pools = h.rados_pools().map_err(|e| e.to_string())?;
+    debug!("pools: {:?}", pools);
 
     for p in pools {
         debug!("Getting an ioctx to: {}", p);
-        let i = get_rados_ioctx(h, &p).map_err(|e| e.to_string())?;
+        let i = h.get_rados_ioctx(&p).map_err(|e| e.to_string())?;
         debug!("Running stat against the pool");
-        let pool_stats = rados_stat_pool(i).map_err(|e| e.to_string())?;
-        destroy_rados_ioctx(i);
+        let pool_stats = i.rados_stat_pool().map_err(|e| e.to_string())?;
         pool_usage.push(PoolInfo {
             name: p.clone(),
             usage: pool_stats,
-            pool_size: get_pool_size(h, &p)?,
+            pool_size: get_pool_size(&h, &p)?,
         });
     }
-    disconnect_from_ceph(h);
     Ok(UsageInfo {
         cluster_usage: cluster_stats,
         pool_usage: pool_usage,
